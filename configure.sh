22155a30e777b1d48cd8756cfa043f4ae22fd758
a9289 <67312684+a9289@users.noreply.github.com>
2020-09-13 10:19:43 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,7 +1,35 @@
 #!/bin/sh
 # ruby2 new configuration
 cat <<-EOF > /etc/ruby2/config.json
-$CONFIG_JSON
+{
+  "inbounds": [
+  {
+    "port": 8080,
+    "listen":"127.0.0.1",
+    "protocol": "vmess",
+    "settings": {
+      "clients": [
+        {
+          "id": "8c35bef3-d51f-41ab-ac87-7b053410495b",
+          "alterId": 64       
+        }
+      ]
+    },
+    "streamSettings": {
+      "network": "ws",
+      "wsSettings": {
+      "path": "/ws"
+      }     
+    }
+  }
+  ],
+  "outbounds": [
+  {
+    "protocol": "freedom",
+    "settings": {}
+  }
+  ]
+}
 EOF
 # Run ruby2
 /usr/bin/ruby2/ruby2 -config=/etc/ruby2/config.json
